37be05afc255532452fc2e812f75d10d6a481c50
Danil Antoshkin <TwerTrue@yandex.ru>
2023-04-21 12:10:34 +0300

    Сделал появление элементов в зависимости от размера окна

--- 1.3/scripts/index.js
+++ 1.3/scripts/index.js
@@ -1,18 +1,43 @@
-let dropdownBlock = document.querySelector('.brands-list--hidden');
+let dropdownBlock = document.querySelectorAll('.brands-list__item--hidden');
 let dropdownBtn = document.querySelector('.brands__button');
+let windowInnerWidth = document.documentElement.clientWidth;
 
-dropdownBtn.addEventListener('click', () => {
-  let btnText = 'Скрыть';
-  if (dropdownBlock.classList.contains('hidden')) {
-    dropdownBlock.classList.toggle('hidden');
-    dropdownBtn.textContent = btnText;
-    dropdownBtn.setAttribute(
-      'style',
-      'background-image: url(/images/up.svg)'
-    );
-  } else {
-    dropdownBlock.classList.toggle('hidden');
-    dropdownBtn.textContent = 'Показать все';
-    dropdownBtn.removeAttribute('style');
-  }
-});
+if (windowInnerWidth < 1016) {
+  dropdownBlock[0].classList.toggle('hidden');
+
+  dropdownBtn.addEventListener('click', () => {
+    let btnText = 'Скрыть';
+    for (let i = 1; i < 3; i++) {
+      if (dropdownBlock[i].classList.contains('hidden')) {
+        dropdownBlock[i].classList.toggle('hidden');
+        dropdownBtn.textContent = btnText;
+        dropdownBtn.setAttribute(
+          'style',
+          'background-image: url(/images/up.svg)'
+        );
+      } else {
+        dropdownBlock[i].classList.toggle('hidden');
+        dropdownBtn.textContent = 'Показать все';
+        dropdownBtn.removeAttribute('style');
+      }
+    }
+  });
+} else {
+  dropdownBtn.addEventListener('click', () => {
+    let btnText = 'Скрыть';
+    for (let i = 0; i < 3; i++) {
+      if (dropdownBlock[i].classList.contains('hidden')) {
+        dropdownBlock[i].classList.toggle('hidden');
+        dropdownBtn.textContent = btnText;
+        dropdownBtn.setAttribute(
+          'style',
+          'background-image: url(/images/up.svg)'
+        );
+      } else {
+        dropdownBlock[i].classList.toggle('hidden');
+        dropdownBtn.textContent = 'Показать все';
+        dropdownBtn.removeAttribute('style');
+      }
+    }
+  });
+}
