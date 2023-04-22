31cbd1480b0436e43726380535e8f18ed9160d1f
Danil Antoshkin <TwerTrue@yandex.ru>
2023-04-22 20:09:43 +0300

    Сделал блок цен на услуги

--- 1.6/src/index.js
+++ 1.6/src/index.js
@@ -2,5 +2,6 @@ import './index.html';
 import './index.scss';
 import './modules/brands_cards';
 import './modules/tech_cards';
+import './modules/prices_cards';
 import './modules/dropdown_brands';
 import './modules/dropdown_tech';
